d2658bd1260d9c72b716c12eafabae035758169f
teovlt <teo.villet2@gmail.com>
2025-06-27 08:48:16 +0200

    Fix responsive translation ...

--- src/components/customs/footer.tsx
+++ src/components/customs/footer.tsx
@@ -1,14 +1,18 @@
 import { Github } from "lucide-react";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
+  const { t } = useTranslation();
 
   return (
     <footer className="bg-white dark:bg-black border-t border-gray-200 dark:border-neutral-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <p className="text-sm text-gray-500 dark:text-neutral-400">©{currentYear} YourCompany. All rights reserved.</p>
+          <p className="text-sm text-gray-500 dark:text-neutral-400">
+            ©{currentYear} {t("footer.rights")}
+          </p>
           <Link
             to="https://github.com/teovlt"
             target="_blank"
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,5 +3,5 @@ import { useTranslation } from "react-i18next";
 export const Home = () => {
   const { t } = useTranslation();
 
-  return <>{t("home")}</>;
+  return <>{t("pages.home.home")}</>;
 };
